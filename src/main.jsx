4804d96323367b48bd927ca5bb52b7705eb1b28a
Ayanleh Moussa Said <ayanleh_moussa.said@sct.gouv.qc.ca>
2026-05-20 08:44:25 -0400

    verifier clerk

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,9 @@ import Settings from './Settings.jsx';
 import Layout from './Layout.jsx';
 
 import './index.css';
-
+<div style={{ color: "white" }}>
+  TEST APP
+</div>
 const clerkPubKey =
   import.meta.env
     .VITE_CLERK_PUBLISHABLE_KEY;
